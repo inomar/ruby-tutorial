c8360d8bf7cd22d5c57c1befcc8e4b64dd120fa5
Makoto Inomata <inomar1486@gmail.com>
2017-07-07 00:43:35 +0900

    add supper class

---
+++ chapter2/supeer_class.rb
@@ -0,0 +1,16 @@
+class Parent
+	def hello
+		puts 'hello, parent class'
+	end
+end
+
+class Child < Parent
+
+	def hi
+		puts 'hello, child class'
+	end
+end
+
+child = Child.new
+child.hi
+child.hello
