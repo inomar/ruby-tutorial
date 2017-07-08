ffd3b62715ea13d2a6e9f73a11c36ca25bf92964
Makoto Inomata <inomar1486@gmail.com>
2017-07-09 00:19:15 +0900

    add string_sample

---
+++ chapter2/module_sample.rb
@@ -0,0 +1,13 @@
+module Brainfsck
+	class Test
+	end
+end
+
+module Whitespace
+	class Test
+	end
+end
+
+# ::を用いる
+Brainfsck::Test
+Whitespace::Test
---
+++ chapter2/string_sample.rb
@@ -0,0 +1,6 @@
+para = "Fear is the path to the dark side.\
+Fear leads to anger.\
+Anger leads to hate. \
+Hate leads to suffering."
+
+puts para
