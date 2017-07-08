261d3912390144f67fdd88854815ac0062c56d1f
Makoto Inomata <inomar1486@gmail.com>
2017-07-09 00:50:48 +0900

    update

---
+++ chapter2/array_sample.rb
@@ -0,0 +1,15 @@
+ary = ['Alice', 4423, 3.14, nil, false]
+
+ary.each do |i|
+	puts i
+end
+
+ary2 = %w(head body foot)
+ary2.each do |i|
+	puts i
+end
+
+ary3 = %i(makoto inomar dra)
+ary3.each do |i|
+	puts i
+end
--- chapter2/string_sample.rb
+++ chapter2/string_sample.rb
@@ -4,3 +4,19 @@ Anger leads to hate. \
 Hate leads to suffering."
 
 puts para
+
+str = <<EOS
+Our meeting was not a coincidence. 
+Nothing happens by accident.
+EOS
+
+puts str
+
+message = 'May the Force be with you.'
+str2 = <<'EOS'
+	#{message}
+EOS
+
+puts str2
+puts %q(#{message})
+puts %Q(#{message})
